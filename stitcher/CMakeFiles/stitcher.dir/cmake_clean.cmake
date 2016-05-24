8729265bd74c91c2373b7977128328111405a9fd
kingofyeti <kingofyeti@gmail.com>
2016-05-24 18:09:01 -0500

    first commit

---
+++ stitcher/CMakeFiles/stitcher.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stitcher.dir/stitching_detailed.cpp.o"
+  "stitcher.pdb"
+  "stitcher"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stitcher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
